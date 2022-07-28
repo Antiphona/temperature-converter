afa1ff14a89dc2087649aa6be0cbc79cbdb65d2b
Antiphona <fermosa.formiga@gmail.com>
2022-07-28 02:20:38 -0500

    First commit of the temperature converter

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//** Convert Celsius to Fahrenheit */
+document.querySelector('#convertC2F').addEventListener('click', convertC2F)
+
+function convertC2F() {
+    const c2F = document.querySelector('#c2FInput').value
+    const c2FResult = c2F * 9 / 5 + 32
+
+    document.querySelector('#c2FOutput').innerText = +parseFloat(c2FResult.toFixed(2))
+}
+
+//**Convert Fahrenheit to Celsius */
+document.querySelector('#convertF2C').addEventListener('click', convertF2C)
+
+function convertF2C() {
+    const f2C = document.querySelector('#f2CInput').value
+    const f2CResult = (f2C - 32) * 5 / 9
+
+    document.querySelector('#f2COutput').innerText = +parseFloat(f2CResult.toFixed(2))
+}
